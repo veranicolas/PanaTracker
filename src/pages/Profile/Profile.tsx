f4b2ff7d3351bdf7b9f5eb49cd1b49a7dd18ec20
Nicolas Vera <nicolasveraa97@gmail.com>
2022-08-27 23:29:26 -0300

    Add shadow component to profileIcon

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,38 +1,47 @@
-import { StyleSheet, Text, View, Image, ImageBackground } from "react-native"
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions } from "react-native"
 import { useSelector } from "react-redux"
 import { CustomButton } from "../../components/CustomButton/CustomButton"
 import { ProfileProps } from "../../models/Props"
+
+import { Shadow } from "react-native-shadow-2"
+
+const DEVICE_HEIGHT = Dimensions.get('screen').height
 
 export const Profile = ({ navigation }:ProfileProps) =>{
 
     const summonerData = useSelector((state:any)=> state.profileData.summonerData)
     const splashData = useSelector((state:any)=> state.mainChampionSplash.splash)
 
-    console.log(splashData)
-
     const onLogoutPress = () =>{
-        navigation.navigate('Login')
+        navigation.navigate('SignUp')
     }
-
-    // TODO set background image as the main champion
 
     return (
         <View style={styles.mainContainer}>
             <ImageBackground 
                 resizeMode="cover"
-                style={styles.backgroundImage}
+                style={[styles.backgroundImage, {height:DEVICE_HEIGHT * 0.3}]}
                 source={{uri:splashData}}
             >  
-                <View style={{height:125, width:125, backgroundColor:'white', flexDirection:'column', justifyContent:'center',alignItems:'center', borderRadius:12, marginLeft:10}}>
-                    <Image 
-                        style={{height:80, width:80}}
-                        source={{uri:`http://ddragon.leagueoflegends.com/cdn/12.16.1/img/profileicon/${summonerData.profileIconId}.png`}}
-                    />
-                    <Text>{summonerData.name}</Text>
+                <View style={styles.profileIcon}>
+                    <Shadow distance={5} startColor="#e3dfde" style={{height:125,width:125, borderRadius:12}}>
+                        <View style={{flexDirection:'column', justifyContent:'center', alignItems:'center', height:'100%', width:'100%'}}>
+                            <Image 
+                                style={{height:80, width:80, borderRadius:5}}
+                                source={{uri:`http://ddragon.leagueoflegends.com/cdn/12.16.1/img/profileicon/${summonerData.profileIconId}.png`}}
+                            />
+                            <Text style={{overflow:'scroll'}}>{summonerData.name}</Text>
+                        </View>
+                    </Shadow>
                 </View>
             </ImageBackground>
 
-            <CustomButton title="Logout" onPress={onLogoutPress}/>
+            <View style={[{height:DEVICE_HEIGHT * 0.55}, styles.informationContainer]}>
+                <View style={{height:'80%', width:'100%'}}>
+
+                </View>
+                <CustomButton title="Logout" onPress={onLogoutPress}/>
+            </View>
         </View>
     )
 }
@@ -50,8 +59,23 @@ const styles = StyleSheet.create({
     backgroundImage:{
         backgroundColor:'white', 
         width:'100%', 
-        height:250, 
         justifyContent:'center', 
         alignItems:'flex-start'
+    },
+    profileIcon:{
+        height:125, 
+        width:125, 
+        backgroundColor:'white', 
+        flexDirection:'column', 
+        justifyContent:'center',
+        alignItems:'center', 
+        borderRadius:12, 
+        marginLeft:10,
+        marginTop:20
+    },
+    informationContainer:{
+        width:'90%', 
+        padding:20,
+        marginTop:10
     }
 })
